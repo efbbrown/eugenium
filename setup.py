8cbdda8086d95f913ae24d3c476f47f51803675b
Eugene Brown <eugene.brown@annalect.com>
2021-04-06 17:22:38 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Eugenium',
+    version='0.0.1',
+    description='Reusable selenium classes written by eugene',
+    author='Eugene Brown',
+    author_email='efbbrown@gmail.com',
+    url='https://github.com/efbbrown/eugenium',
+    packages=['eugenium'])
