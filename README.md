e93734d1a9e0779a9209cf04f63c8de5a20fea7a
Eugene Brown <eugene.brown@annalect.com>
2021-06-07 21:15:32 +0100

    Initial version of package added to repository

---
+++ README.md
@@ -0,0 +1,15 @@
+# Eugenium
+
+Utilities to create and deploy selenium processes on google cloud platform
+
+## Requirements
+
+To develop your selenium webscraper, you'll need to download the chrome
+webdriver for your operating system. You can do so
+here <https://chromedriver.chromium.org/downloads>.
+
+## Installation
+
+```
+pip install git+git://github.com/efbbrown/eugenium.git
+```
--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from distutils.core import setup
 setup(
     name='Eugenium',
     version='0.0.1',
-    description='Reusable selenium classes written by eugene',
+    license='MIT',
+    description='Classes to create and deploy selenium processes on google cloud platform',
     author='Eugene Brown',
     author_email='efbbrown@gmail.com',
     url='https://github.com/efbbrown/eugenium',
